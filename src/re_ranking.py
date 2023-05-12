ea4d23cba5e10c5902fb7107d4b894d7e04745cf
Paul Erpenstein <paul.erpenstein@gmail.com>
2023-05-12 14:08:20 +0200

    extract validation party from training script

--- src/re_ranking.py
+++ src/re_ranking.py
@@ -144,19 +144,3 @@ for epoch in range(10):
         no_improvement_since_last_epoch = metrics[-1]['MRR@10'] < metrics[-2]['MRR@10']
         if no_improvement_since_last_epoch:
             break
-
-
-# eval (duplicate for validation inside train loop - but rename "loader", since
-# otherwise it will overwrite the original train iterator, which is instantiated outside the loop)
-#
-
-_tuple_reader = IrLabeledTupleDatasetReader(
-    lazy=True, max_doc_length=180, max_query_length=30)
-_tuple_reader = _tuple_reader.read(config.test_data)
-_tuple_reader.index_with(vocab)
-loader = PyTorchDataLoader(_tuple_reader, batch_size=128)
-
-for batch in Tqdm.tqdm(loader):
-    # todo test loop
-    # todo evaluation
-    pass
